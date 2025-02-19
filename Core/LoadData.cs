9b4bbfa3115aa1c2b2f3094b715ca54bbce89404
1R1an1 <iangamer7764@gmail.com>
2025-02-19 16:57:59 -0300

    cambiando nombre de LoadDataLogic a SavesDataLogic y moviendo esa clase a otro archivo

--- Core/LoadData.cs
+++ Core/LoadData.cs
@@ -3,7 +3,6 @@
 using FortiCrypts;
 using System;
 #endif
-using System.Collections.Generic;
 using System.IO;
 
 namespace Rain_save_manager.Core
@@ -11,11 +10,12 @@ namespace Rain_save_manager.Core
     public static class LoadData
     {
         public static SavesData savesData;
+
         public static void Start()
         {
             ComprobarData<SavesData>(out var result);
             savesData = result;
-            LoadDataLogic.VerifyInvalidSaves();
+            SavesDataLogic.VerifyInvalidSaves();
 
             #if DEBUG
             ConfigSystem.WriteConfigFile(SavesData.fileName, savesData);
@@ -23,7 +23,6 @@ namespace Rain_save_manager.Core
             #endif
 
         }
-
         public static void Close()
         {
             ConfigSystem.WriteConfigFile(SavesData.fileName, savesData);
@@ -33,49 +32,6 @@ namespace Rain_save_manager.Core
         {
             try { result = ConfigSystem.ReadConfigFile<T>(); }
             catch (FileNotFoundException) { result = new T(); ConfigSystem.WriteConfigFile(typeof(T).Name, result); }
-        }
-    }
-    
-    public static class LoadDataLogic
-    {
-        public static void VerifyInvalidSaves()
-        {
-            int exist = 0;
-            for (int i = 0; i <= LoadData.savesData.Saves.Count; i++)
-            {
-                if (i >= LoadData.savesData.Saves.Count)
-                    break;
-                if (exist == 0 && i != 0)
-                    i--;
-                if (!File.Exists(Path.Combine(App.appsaves, $"sav-{LoadData.savesData.Saves[i].saveId}")))
-                {
-                    LoadData.savesData.Saves.Remove(LoadData.savesData.Saves[i]);
-                    if (i != 0)
-                        i--;
-                }
-                else
-                    exist++;
-            }
-        }
-        public static SaveData FindSaveDataForId(int id)
-        {
-            for (int i = 0; i < LoadData.savesData.Saves.Count; i++)
-            {
-                if (LoadData.savesData.Saves[i].saveId == id)
-                    return LoadData.savesData.Saves[i];
-            }
-
-            throw new KeyNotFoundException($"El id: '{id}' no fue encontrado en la lista.");
-        }
-        public static int FindSaveDataForIdInSaves(int id)
-        {
-            for (int i = 0; i < LoadData.savesData.Saves.Count; i++)
-            {
-                if (LoadData.savesData.Saves[i].saveId == id)
-                    return i;
-            }
-
-            throw new KeyNotFoundException($"El elemento con el id: '{id}' no fue encontrado en la lista.");
         }
     }
 }
---
+++ Core/SavesDataLogic.cs
@@ -0,0 +1,65 @@
+﻿using Rain_save_manager.Model;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Rain_save_manager.Core
+{
+    public static class SavesDataLogic
+    {
+        public static void VerifyInvalidSaves()
+        {
+            int exist = 0;
+            for (int i = 0; i <= LoadData.savesData.Saves.Count; i++)
+            {
+                if (i >= LoadData.savesData.Saves.Count)
+                    break;
+                if (exist == 0 && i != 0)
+                    i--;
+                if (!File.Exists(Path.Combine(App.appsaves, $"sav-{LoadData.savesData.Saves[i].saveId}")))
+                {
+                    LoadData.savesData.Saves.Remove(LoadData.savesData.Saves[i]);
+                    if (i != 0)
+                        i--;
+                }
+                else
+                    exist++;
+            }
+        }
+        public static SaveData FindSaveDataForId(int id)
+        {
+            for (int i = 0; i < LoadData.savesData.Saves.Count; i++)
+            {
+                if (LoadData.savesData.Saves[i].saveId == id)
+                    return LoadData.savesData.Saves[i];
+            }
+
+            throw new KeyNotFoundException($"El id: '{id}' no fue encontrado en la lista.");
+        }
+        public static int FindSaveDataForIdInSaves(int id)
+        {
+            for (int i = 0; i < LoadData.savesData.Saves.Count; i++)
+            {
+                if (LoadData.savesData.Saves[i].saveId == id)
+                    return i;
+            }
+
+            throw new KeyNotFoundException($"El elemento con el id: '{id}' no fue encontrado en la lista.");
+        }
+        public static int GetMaxIdInSaves()
+        {
+            if (LoadData.savesData.Saves.Count == 0)
+                return -1;
+
+            if (LoadData.savesData.Saves.Count == 1)
+                return LoadData.savesData.Saves[0].saveId;
+
+            int maxId = 0;
+            for (int i = 0; i < LoadData.savesData.Saves.Count; i++)
+            {
+                if (maxId < LoadData.savesData.Saves[i].saveId)
+                    maxId = LoadData.savesData.Saves[i].saveId;
+            }
+            return maxId;
+        }
+    }
+}
