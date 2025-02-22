b6ee6d5eafcf8191d6c6db60b3fa8187b867b13b
1R1an1 <iangamer7764@gmail.com>
2025-02-22 14:18:42 -0300

    añadiendo valores a el enum save

--- Model/Enums.cs
+++ Model/Enums.cs
@@ -4,7 +4,7 @@
     {
         public enum RSMD { Config, Saves }
         public enum OWT { RenemeSaves, ReplaceSave }
-        public enum Save { Save_1, Save_2, Save_3 }
+        public enum Save { Save_1 = 1, Save_2 = 2, Save_3 = 3 }
 
     }
 }
