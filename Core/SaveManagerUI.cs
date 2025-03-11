2480bd6291d7ac3717453b85f58563a35c300e5e
1R1an1 <iangamer7764@gmail.com>
2025-03-11 02:39:35 -0300

    Eliminando CreateSaveLabel y CreateContextMenuSave, añadiendo metodos para desabilitar y habilitar los botones y otras cosas

--- Core/SaveManagerUI.cs
+++ Core/SaveManagerUI.cs
@@ -1,137 +1,110 @@
 ﻿using Rain_save_manager.Model;
 using System.Windows.Controls;
-using System.Windows.Media;
 using System.Windows;
-using Rain_save_manager.Views;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+using Rain_save_manager.Views;
 
 namespace Rain_save_manager.Core
 {
     public class SaveManagerUI
     {
-        private StackPanel _SP_saves;
-        private Dictionary<int, Label> _lblSaves;
-        private Label _lblSave1, _lblSave2, _lblSave3;
+        private Dictionary<int, RadioButton> _RB_Saves;
+        private int _RB_Visual_Count;
+        private bool _Is_btn_enabled = true;
         private MainView _mainView;
 
-        public SaveManagerUI(StackPanel _SP_saves, Label _lblSave1, Label _lblSave2, Label _lblSave3, MainView _mainView)
+        public SaveManagerUI(MainView _mainView)
         {
-            this._SP_saves = _SP_saves;
-            _lblSaves = new Dictionary<int, Label>();
-            this._lblSave1 = _lblSave1;
-            this._lblSave2 = _lblSave2;
-            this._lblSave3 = _lblSave3;
+            _RB_Saves = new Dictionary<int, RadioButton>();
             this._mainView = _mainView;
+            VerifySelectedRadioButton();
         }
 
-        public void InitializeLabelsSaves()
+
+        private void VerifySelectedRadioButton() => SetIsEnabledInButtons(_RB_Saves.Values.Any(rb => rb.IsChecked == true));
+        private void SetButtonProperties(Button button, bool isEnabled, string tagResource, string foregroundResource)
         {
-            _lblSave1.ContextMenu = CreateContextMenuSave(Enums.Save.Save_1);
-            _lblSave1.MouseDoubleClick += _mainView.btn_cpysave1_Click;
-            
-            _lblSave2.ContextMenu = CreateContextMenuSave(Enums.Save.Save_2);
-            _lblSave2.MouseDoubleClick += _mainView.btn_cpysave2_Click;
-            
-            _lblSave3.ContextMenu = CreateContextMenuSave(Enums.Save.Save_3);
-            _lblSave3.MouseDoubleClick += _mainView.btn_cpysave3_Click;
+            button.IsEnabled = isEnabled;
+            button.Tag = (Brush)App.Current.FindResource(tagResource);
+            button.Foreground = (Brush)App.Current.FindResource(foregroundResource);
+        }
+        private void SetIsEnabledInButtons(bool enable)
+        {
+            if (_Is_btn_enabled == enable)
+                return;
+            string tagResource = enable ? "NormalColorNormal" : "NormalColorDisabled";
+            string foregroundResource = enable ? "FontColor" : "FontColorDisabled";
 
-
-            foreach (KeyValuePair<int, SaveData> dictionary in LoadData.savesData.Saves)
+            SetButtonProperties(_mainView.btn_Eliminar, enable, tagResource, foregroundResource);
+            SetButtonProperties(_mainView.btn_CambiarNombre, enable, tagResource, foregroundResource);
+            SetButtonProperties(_mainView.btn_Utilizar, enable, tagResource, foregroundResource);
+            SetButtonProperties(_mainView.btn_Actualizar, enable, tagResource, foregroundResource);
+            _Is_btn_enabled = enable;
+        }
+        private void ActualizarMarginRadioButtons()
+        {
+            for (int i = 0; i < _RB_Saves.Count; i++)
             {
-                Label lbl = CreateSaveLabel(dictionary);
+                List<RadioButton> rb = new List<RadioButton>(_RB_Saves.Values);
 
-                _lblSaves.Add(dictionary.Key, lbl);
-                _SP_saves.Children.Add(lbl);
+                double rightMargin = (i % 2 == 0) ? 10 : 0;
+                double bottomMargin = (i >= rb.Count - (_RB_Saves.Count % 2 != 0 ? 1 : 2)) ? 0 : 10;
+
+                rb[i].Margin = new Thickness(0, 0, rightMargin, bottomMargin);
             }
         }
-        public Label CreateSaveLabel(KeyValuePair<int, SaveData> save)
+
+
+        public RadioButton CreateSaveRadioButton(KeyValuePair<int, SaveData> save)
         {
-            Label lbl = new Label()
+            _RB_Visual_Count++;
+            RadioButton rb = new RadioButton()
             {
                 Name = $"id_{save.Key}",
-                Style = (Style)App.Current.FindResource("lblS"),
+                Style = (Style)App.Current.FindResource("RadioButtonStyle"),
                 Content = save.Value.saveName,
+                VerticalContentAlignment = VerticalAlignment.Center,
+                Padding = new Thickness(5.5),
+                MaxWidth = 290.5,
+                Width = 290.5,
                 FontSize = 13.5,
-                Margin = new Thickness(0,0,0,2.5),
-                ContextMenu = CreateContextMenu(save.Key)
-            };
-            lbl.MouseDoubleClick += (s, e) => _mainView.RemplazarSave_Click(s, e, save.Key);
-            return lbl;
-        }
-        private ContextMenu CreateContextMenu(int saveId)
-        {
-            ContextMenu contextMenu = new ContextMenu() { Style = (Style)App.Current.FindResource("CM") };
-
-            MenuItem renameItem = new MenuItem()
-            {
-                FontSize = 12,
-                FontFamily = new FontFamily("Consolas"),
-                Style = (Style)App.Current.FindResource("MIUP"),
-                Header = "Cambiar nombre"
-            };
-            renameItem.Click += (s, e) => _mainView.CambiarNombre_Click(s, e, saveId);
-
-            MenuItem UpdateItem = new MenuItem()
-            {
-                FontSize = 12,
-                FontFamily = new FontFamily("Consolas"),
-                Style = (Style)App.Current.FindResource("MIMIDDLE"),
-                Header = "Actualizar"
-            };
-            UpdateItem.Click += (s, e) => _mainView.Update_Click(s, e, saveId);
-
-            MenuItem deleteItem = new MenuItem()
-            {
-                FontSize = 12,
-                FontFamily = new FontFamily("Consolas"),
-                Style = (Style)App.Current.FindResource("MIDOWN"),
-                Header = "Eliminar"
-            };
-            deleteItem.Click += (s, e) => _mainView.Eliminar_Click(s, e, saveId);
-
-            contextMenu.Items.Add(renameItem);
-            contextMenu.Items.Add(UpdateItem);
-            contextMenu.Items.Add(deleteItem);
-            return contextMenu;
-        }
-
-        private ContextMenu CreateContextMenuSave(Enums.Save save)
-        {
-            ContextMenu contextMenu = new ContextMenu() { Style = (Style)App.Current.FindResource("CM") };
-
-            MenuItem copiSave = new MenuItem()
-            {
-                FontSize = 12,
-                FontFamily = new FontFamily("Consolas"),
-                Style = (Style)App.Current.FindResource("MI"),
-                Header = "Copiar"
+                GroupName = "Saves"
             };
 
-            if (save == Enums.Save.Save_1)
-                copiSave.Click += _mainView.btn_cpysave1_Click;
-            else if (save == Enums.Save.Save_2)
-                copiSave.Click += _mainView.btn_cpysave2_Click;
-            else
-                copiSave.Click += _mainView.btn_cpysave3_Click;
+            rb.Margin = _RB_Visual_Count < 2 ? new Thickness(0, 0, 10, 10) : new Thickness(0, 0, 0, 10);
+            _RB_Visual_Count = _RB_Visual_Count < 2 ? _RB_Visual_Count : 0;
 
-            contextMenu.Items.Add(copiSave);
-            return contextMenu;
+            rb.Checked += (s, e) => SetIsEnabledInButtons(true);
+            //lbl.MouseDoubleClick += (s, e) => _mainView.RemplazarSave_Click(s, e, save.Key);
+            return rb;
         }
+        public KeyValuePair<int, RadioButton> GetSelectedRadioButton() => _RB_Saves.FirstOrDefault(rb => rb.Value.IsChecked == true);
 
-        public void AddLabel(KeyValuePair<int, Label> lbl)
+
+        public void InitializeRadioButtonSaves() { foreach (KeyValuePair<int, SaveData> dictionary in LoadData.savesData.Saves) { AddRadioButton(new KeyValuePair<int, RadioButton>(dictionary.Key, CreateSaveRadioButton(dictionary))); } }
+        public void AddRadioButton(KeyValuePair<int, RadioButton> rb)
         {
-            _SP_saves.Children.Add(lbl.Value);
-            _lblSaves.Add(lbl.Key, lbl.Value);
-        }
-        public void ActualizarLabel(int id)
-        {
-            _lblSaves[id].Content = LoadData.savesData.Saves[id].saveName;
+            _mainView.WP_saves.Children.Add(rb.Value);
+            _RB_Saves.Add(rb.Key, rb.Value);
+            ActualizarMarginRadioButtons();
         }
 
-        public void EliminarLabel(int id)
+
+        public void ActualizarRadioButton(int id)
         {
-            _SP_saves.Children.Remove(_lblSaves[id]);
-            _lblSaves.Remove(id);
+            _RB_Saves[id].Content = LoadData.savesData.Saves[id].saveName;
         }
+        public void EliminarRadioButton(int id)
+        {
+            _mainView.WP_saves.Children.Remove(_RB_Saves[id]);
+            _RB_Saves.Remove(id);
+            if (_RB_Visual_Count != 0)
+                _RB_Visual_Count--;
+            ActualizarMarginRadioButtons();
+            VerifySelectedRadioButton();
+        }
+        public void VerificarScrollbar() => _mainView.WP_saves.Margin = _mainView.SV_saves.ComputedVerticalScrollBarVisibility == Visibility.Visible ? new Thickness(0, 0, 5, 0) : new Thickness(10, 0, 10, 0);
     }
 }
