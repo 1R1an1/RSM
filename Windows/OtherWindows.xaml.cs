aec18c33f2e4bee9adc5d522eb4b83b9de734b18
1R1an1 <iangamer7764@gmail.com>
2025-03-13 21:08:26 -0300

    haciendo que las view de OtherWindows se inicializen en el codigo c#

--- Windows/OtherWindows.xaml.cs
+++ Windows/OtherWindows.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using Rain_save_manager.Model;
+using Rain_save_manager.Views;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Rain_save_manager.Windows
 {
     public partial class OtherWindows : Window
     {
+        public RenameSavesView CDU_RENS;
+        private ReplaceSaveView CDU_REPS;
         public string texto;
         public Enums.Save save;
         public OtherWindows(Enums.OWT Type, string title = "")
@@ -13,6 +17,10 @@ namespace Rain_save_manager.Windows
             InitializeComponent();
             if (Type == Enums.OWT.RenemeSaves)
             {
+                CDU_RENS = new RenameSavesView();
+                Grid.SetRow(CDU_RENS, 1);
+                grid.Children.Add(CDU_RENS);
+
                 string titulo = title == "" ? "Renombrar partida (RSM)" : title + " (RSM)";
                 this.Title = titulo;
                 lbl_title.Content = titulo;
@@ -21,6 +29,10 @@ namespace Rain_save_manager.Windows
             }
             else if (Type == Enums.OWT.ReplaceSave)
             {
+                CDU_REPS = new ReplaceSaveView();
+                Grid.SetRow(CDU_REPS, 1);
+                grid.Children.Add(CDU_REPS);
+
                 string titulo = title == "" ? "Remplazar partida (RSM)" : title + " (RSM)";
                 this.Title = titulo;
                 lbl_title.Content = titulo;
