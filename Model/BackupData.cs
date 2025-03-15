6c57610f9541a501f5794c30c4890c7e6147d885
1R1an1 <iangamer7764@gmail.com>
2025-03-15 00:53:10 -0300

    Añadiendo el model para Backups

---
+++ Model/BackupData.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Rain_save_manager.Model
+{
+    public class BackupData
+    {
+        public DateTime time;
+        [JsonProperty("timeString")]
+        public string timeS;
+        //public Enums.BackupType backupType;
+
+        public BackupData(DateTime time) { this.time = time; timeS = string.Empty; timeS = time.ToString("dd/MM/yyyy__HH:mm:ss:fff"); }
+    }
+}
---
+++ Model/BackupsData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Rain_save_manager.Model
+{
+    public class BackupsData : ConfigBehaviour
+    {
+        public Dictionary<int, BackupData> Backups = new Dictionary<int, BackupData>();
+
+        public BackupsData(Dictionary<int, BackupData> Backups) : base(typeof(BackupsData).Name) { this.Backups = Backups; }
+        public BackupsData() : base(typeof(BackupsData).Name) { }
+    }
+}
