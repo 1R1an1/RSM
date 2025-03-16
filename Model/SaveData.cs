fe4acb44f9d77d3fc409cbaa0465c3c51c7c6e95
1R1an1 <iangamer7764@gmail.com>
2025-03-16 19:41:44 -0300

    Modificando SaveData y eliminando SavesData y ConfigBehaviour

--- Model/SaveData.cs
+++ Model/SaveData.cs
@@ -1,10 +1,24 @@
-﻿namespace Rain_save_manager.Model
+﻿using Newtonsoft.Json;
+
+namespace Rain_save_manager.Model
 {
     public class SaveData
     {
-        public string saveName;
-        public string saveFileName;
+        [JsonProperty("Id")]
+        public int saveId;
 
-        public SaveData(string saveName, string saveFileName) { this.saveName = saveName; this.saveFileName = saveFileName; }
+        [JsonProperty("Name")]
+        public string saveName = string.Empty;
+
+        [JsonProperty("Content")]
+        public string saveContent = string.Empty;
+
+        public SaveData(string saveName, int saveId, string saveContent)
+        {
+            this.saveName = saveName;
+            this.saveId = saveId;
+            this.saveContent = saveContent;
+        }
+        public SaveData() { }
     }
 }
